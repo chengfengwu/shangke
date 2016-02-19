aa36b2d787553a07c96c1049ee44e23fa4f33623
wind <693227477@qq.com>
2016-02-19 14:01:42 +0800

    first
    添加项目

---
+++ shangke/shangke/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  shangke
+//
+//  Created by Wind on 16-2-19.
+//  Copyright (c) 2016年 Wind. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"老是搞不了的电脑");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
